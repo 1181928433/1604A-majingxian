804d93bdefe466a95e0a56777607eeb006d4be40
majingxian <1181928433@qq.com>
2018-11-14 09:02:04 +0800

    fd

---
+++ 11-14/name.php
@@ -0,0 +1,22 @@
+<?php
+class solution{
+	public;
+	int Add(int numl, int num2)
+	{
+		int i = 0;
+		if(num2> 0)
+		{
+			for(i = 0;i <num2;i++)
+			{
+				return num；
+			}else if(num2< 0){
+				for(i = 0;i< -num2;i++)
+			}
+		num-i
+		}
+		return num1;
+	}else{
+		retyrn num1;
+	}
+}
+?>
